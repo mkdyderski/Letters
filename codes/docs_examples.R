c175b73f77fd3b2a9786a66ee5705af140d805bc
Maciej Beręsewicz <maciej.beresewicz@gmail.com>
2016-07-02 23:05:44 +0200

    example codes to run documents

---
+++ codes/docs_examples.R
@@ -0,0 +1,39 @@
+### examples
+
+
+name <- 'Adam'
+surname <- 'Student'
+inst <- 'Poznan University of Economics and Bisiness'
+city <- 'Poznan'
+country <- 'Poland'
+mail <- 'some-mail@domain.com'
+pres_title <- 'the title of the presentation'
+type <- 'presentation'
+
+w <- 2
+work_fee <- ifelse(w == 1,'200 PLN / 50 EUR', '400 PLN / 100 EUR')
+work_names <- c('workshop 1', 'workshop2')
+work_time <- c('morning', 'afternoon')
+work_status <- c('a reserve list', 'a regular list')
+
+
+
+# paper confirmation -------------------------------------------------
+
+knit2pdf(input = 'erum_conference_confirmation.rnw',
+         output = 'erum_conference_confirmation.tex')
+
+# workshop confirmation ---------------------------------------------------
+
+knit2pdf(input = 'erum_workshop_confirmation.rnw',
+         output = 'erum_workshop_confirmation.tex')
+
+# conference conf without presentation  -----------------------------------
+
+knit2pdf(input = 'erum_participation_confirmation.Rnw',
+         output = 'erum_participation_confirmation.tex')
+
+junk <- dir(path=getwd(), pattern="*.(aux|log|out|tex)") 
+file.remove(junk)
+
+
