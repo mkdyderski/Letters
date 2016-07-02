75b5c47cec963e8a17c65c4f1ef05e7317292fc3
Maciej Beręsewicz <maciej.beresewicz@gmail.com>
2016-07-02 16:29:59 +0200

    update conference confirmation

--- codes/docs_script.R
+++ codes/docs_script.R
@@ -7,9 +7,10 @@ library(stringi)
 ## pass arguments to knitr document while compilation
 
 dir.create('template/mails')
-
 setwd('template/mails')
 
+
+# paper confirmation -------------------------------------------------
 
 name <- stri_trans_totitle('name')
 surname <- stri_trans_totitle('surname')
@@ -19,8 +20,20 @@ mail <- 'mail@mail.com'
 pres_title <- stri_trans_toupper('pres title pres title ')
 type <- stri_trans_toupper('type')
 
+
 knit2pdf(input = '../erum_conference_confirmation.rnw',
-         output = paste0(surname,'_erum_conference_confirmation.tex'))
+         output = paste0(surname,'_',name,'_erum_conference_confirmation.tex'))
 
 
-  
+
+# workshop confirmation ---------------------------------------------------
+
+knit2pdf(input = '../erum_workshop_confirmation.rnw',
+         output = paste0(surname,'_workshop_confirmation.tex'))
+
+
+# conference conf without presentation  -----------------------------------
+
+
+knit2pdf(input = '../erum_workshop_confirmation.rnw',
+         output = paste0(surname,'_workshop_confirmation.tex'))
