9fc2ff40d81000085a05b902691d191591a0fea1
Maciej Beręsewicz <maciej.beresewicz@gmail.com>
2016-07-02 19:18:21 +0200

    lists final version

--- codes/docs_script.R
+++ codes/docs_script.R
@@ -9,34 +9,31 @@ library(lubridate)
 
 # read data ---------------------------------------------------------------
 
+
+dir.create('template/mails')
+setwd('template/mails')
+
 my_sheets <- gs_ls() 
 erum <- gs_title('eRum form (Responses)')
 GAP_KEY <- gs_gap_key()
 erum <- erum %>% gs_gs()
 
+# paper confirmation -------------------------------------------------
+
 register <- erum %>% gs_read(ws = 1, col_names = FALSE) %>%
   select(X1:X7,X11,X13,X16) %>%
   rename( how = X11,
-         title = X13,
-        timestamp = X1 ,
-        name = X2 ,
-        surname = X3 ,
-        email = X4 ,
-        institute = X5 ,
-        city = X6 ,
-        country = X7,
-        when = X16) %>%
+          title = X13,
+          timestamp = X1 ,
+          name = X2 ,
+          surname = X3 ,
+          email = X4 ,
+          institute = X5 ,
+          city = X6 ,
+          country = X7,
+          when = X16) %>%
   filter(timestamp != 'Timestamp') %>%
   mutate(timestamp = dmy_hms(timestamp))
-  
-
-## pass arguments to knitr document while compilation
-
-dir.create('template/mails')
-setwd('template/mails')
-
-
-# paper confirmation -------------------------------------------------
 
 register_presentation <- register %>%
   filter(!grepl('without', how)) %>% 
@@ -63,6 +60,8 @@ for (i in 1:nrow(register_presentation)) {
   knit2pdf(input = '../erum_conference_confirmation.rnw',
            output = paste0(gsub('\\s+','_',surname),'_',
                            gsub('\\s+','_',name),'_erum_conference_confirmation.tex'))
+  junk <- dir(path=getwd(), pattern="*.(aux|log|out|tex)") 
+  file.remove(junk)
 }
 
 
@@ -82,23 +81,23 @@ workshp <- erum %>%
 
 data_input <- workshp
 
-for (i in nrow(workshp)) {
+for (i in 1:nrow(data_input)) {
   
   
   name <- stri_trans_totitle(data_input$name[i])
   name <- gsub('[[:punct:]]','',name)
   surname <- stri_trans_totitle(data_input$surname[i])
-  inst <- stri_trans_totitle(data_input$institute[i])
-  city <- stri_trans_totitle(data_input$city[i])
-  city <- ifelse(city == 'Poznań','Poznan',city)
-  country <- stri_trans_totitle(data_input$country[i])
-  mail <- data_input$email[i]
+  inst <- stri_trans_totitle(data_input$df[[i]]$institute)
+  #city <- stri_trans_totitle(data_input$df[[i]]$)
+  #city <- ifelse(city == 'Poznań','Poznan',city)
+  #country <- stri_trans_totitle(data_input$country[i])
+  mail <- data_input$mail[i]
   
   w <- nrow(data_input$df[[i]]) 
   work_fee <- ifelse(w == 1,'200 PLN / 50 EUR', '400 PLN / 100 EUR')
   work_names <- gsub('\t','',stri_trans_totitle(data_input$df[[i]]$workshp))
   work_time <- stri_trans_totitle(data_input$df[[i]]$when)
-  work_status <- stri_trans_totitle(data_input$df[[i]]$status)
+  work_status <- stri_trans_tolower(data_input$df[[i]]$status)
   
   
   knit2pdf(input = '../erum_workshop_confirmation.rnw',
@@ -136,9 +135,10 @@ for (i in 1:nrow(data_input)) {
   knit2pdf(input = '../erum_participation_confirmation.Rnw',
            output = paste0(gsub('\\s+','_',surname),'_',
                            gsub('\\s+','_',name),'_erum_participation_confirmation.tex'))
+  junk <- dir(path=getwd(), pattern="*.(aux|log|out|tex)") 
+  file.remove(junk)
 }
 
-junk <- dir(path=getwd(), pattern="*.(aux|log|out|tex)") 
-file.remove(junk)
+
 
 
