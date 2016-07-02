1463e5f34e76ff5cc5ff3640da3be4f331a05eff
Maciej Beręsewicz <maciej.beresewicz@gmail.com>
2016-07-02 14:08:15 +0200

    template for conference confirmation

--- codes/docs_script.R
+++ codes/docs_script.R
@@ -2,5 +2,25 @@
 library(readxl)
 library(knitr)
 library(dplyr)
+library(stringi)
 
 ## pass arguments to knitr document while compilation
+
+dir.create('template/mails')
+
+setwd('template/mails')
+
+
+name <- stri_trans_totitle('name')
+surname <- stri_trans_totitle('surname')
+inst <- stri_trans_totitle('inst')
+city <- stri_trans_totitle('city')
+mail <- 'mail@mail.com'
+pres_title <- stri_trans_toupper('pres title pres title ')
+type <- stri_trans_toupper('type')
+
+knit2pdf(input = '../erum_conference_confirmation.rnw',
+         output = paste0(surname,'_erum_conference_confirmation.tex'))
+
+
+  
