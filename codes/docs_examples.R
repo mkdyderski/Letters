1fe341d6121970764e3374dec868a1abf7353313
nowosad <tupiszakaczki@gmail.com>
2016-07-04 15:09:11 +0200

    typos and code fixes

--- codes/docs_examples.R
+++ codes/docs_examples.R
@@ -17,18 +17,18 @@ work_status <- c('a reserve list (30/25)', 'a regular list')
 
 # paper confirmation -------------------------------------------------
 
-knit2pdf(input = 'erum_conference_confirmation.rnw',
-         output = 'erum_conference_confirmation.tex')
+knit2pdf(input = 'template/erum_conference_confirmation.rnw',
+         output = 'template/erum_conference_confirmation.tex')
 
 # workshop confirmation ---------------------------------------------------
 
-knit2pdf(input = 'erum_workshop_confirmation.rnw',
-         output = 'erum_workshop_confirmation.tex')
+knit2pdf(input = 'template/erum_workshop_confirmation.Rnw',
+         output = 'template/erum_workshop_confirmation.tex')
 
 # conference conf without presentation  -----------------------------------
 
-knit2pdf(input = 'erum_participation_confirmation.Rnw',
-         output = 'erum_participation_confirmation.tex')
+knit2pdf(input = 'template/erum_participation_confirmation.Rnw',
+         output = 'template/erum_participation_confirmation.tex')
 
-junk <- dir(path=getwd(), pattern="*.(aux|log|out|tex)") 
+junk <- dir(path="template/", pattern="*.(aux|log|out|tex)", full.names = TRUE) 
 file.remove(junk)
